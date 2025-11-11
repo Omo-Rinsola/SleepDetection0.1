ce420a37069030ed0d06b71ec652676c1f8502e6
omorinsolamakinde <rinsola.childwealth@gmail.com>
2025-11-11 17:38:40 +0100

    Updated main and added render build and start command

--- backend/main.py
+++ backend/main.py
@@ -74,5 +74,5 @@ async def websocket_endpoint(websocket: WebSocket):
         print("Client disconnected")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
